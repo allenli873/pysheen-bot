deb894ab2b3b951f715e64b573b9ca04c23015fd
allli <allli@umich.edu>
2020-11-30 16:24:22 -0800

    test

--- cogs/Camel.py
+++ cogs/Camel.py
@@ -12,24 +12,15 @@ class Camel(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.camel.start()
-        self.sampai.start()
 
     def cog_unload(self):
         self.camel.cancel()
-        self.sampai.cancel()
-
 
     @tasks.loop(minutes=5)
     async def camel(self):
         channel = self.bot.get_channel(781629627730231328)
         if channel:
             await channel.send("$camel")
-
-    @tasks.loop(seconds=5)
-    async def sampai(self):
-        channel = self.bot.get_channel(781629627730231328)
-        if channel:
-            await channel.send(f"$userrank <@271689732386324482>{random.randint(1, 9)}")
 
 def setup(bot):
     bot.add_cog(Camel(bot))
