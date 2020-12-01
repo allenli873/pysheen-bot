4fc5346d2c53ac943caa4f4947512869f8774566
allli <allli@umich.edu>
2020-11-30 19:59:59 -0800

    fixed some buggies

--- cogs/Ranks.py
+++ cogs/Ranks.py
@@ -6,41 +6,46 @@ from bs4 import BeautifulSoup
 
 from discord.ext import commands, tasks
 
-CAMEL_RANK_CACHE = get_rank('camellCase#NA1')
 
-def get_rank(name):
-    url = "https://tracker.gg/valorant/profile/riot/" + urllib.parse.quote(name) + "/overview?playlist=competitive"
-    req = requests.get(url)
-    soup = BeautifulSoup(req.content, "html.parser")
-    try:
-        return soup.find_all("span", class_="valorant-highlighted-stat__value")[0].text
-    except:
-        return None
+CAMEL_RANK_CACHE = ''
 
 class Ranks(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.print_camel_rank.start()
+        self.CAMEL_RANK_CACHE = self.get_rank('camellCase#NA1')
+
+    def get_rank(self, name):
+        url = "https://tracker.gg/valorant/profile/riot/" + urllib.parse.quote(name) + "/overview?playlist=competitive"
+        req = requests.get(url)
+        soup = BeautifulSoup(req.content, "html.parser")
+        try:
+            return soup.find_all("span", class_="valorant-highlighted-stat__value")[0].text
+        except:
+            return None
 
     def cog_unload(self):
         self.print_camel_rank.cancel()
 
-    @tasks.loop(minutes=5)
+    @tasks.loop(seconds=5)
     async def print_camel_rank(self):
+        global CAMEL_RANK_CACHE
         channel = self.bot.get_channel(781629627730231328)
-        rank: str = get_rank("camellCase#NA1")
+        if not channel:
+            return
+        rank: str = self.get_rank("camellCase#NA1")
         if rank:
             await channel.send(rank)
-            if CAMEL_RANK_CACHE != rank:
-                CAMEL_RANK_CACHE = rank
+            if CAMEL_RANK_CACHE != rank and len(CAMEL_RANK_CACHE) > 0:
                 await channel.send(f'<@&781636133762629632>, camel rank has changed to {rank} !')
+            CAMEL_RANK_CACHE = rank
         else:
             await channel.send('bad')
 
     @commands.command(name='rank')
     async def rank_of(self, ctx, *args):
         for id in args:
-            rank: str = get_rank(id)
+            rank: str = self.get_rank(id)
             if rank:
                 await ctx.message.channel.send(rank)
             else:
